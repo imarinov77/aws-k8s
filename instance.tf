5a110e0f909542d07a095867e2cff0ed86c6cad5
Ivan Marinov <ivan.marinov@acronis.com>
2022-09-25 20:53:53 +0300

    Added nfs server

--- instance.tf
+++ instance.tf
@@ -11,6 +11,19 @@ resource "aws_instance" "k8s-master" {
   tags = { Name = "k8s-master" }
 }
 
+resource "aws_instance" "k8s-nfs" {
+  ami           = var.amis[var.aws_region]
+  instance_type = "t2.micro"
+  key_name = "aws-key"
+  vpc_security_group_ids = [aws_security_group.k8s.id]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 20
+    volume_type = "gp2"
+  }
+  tags = { Name = "k8s-nfs" }
+}
+
 resource "aws_instance" "k8s-worker" {
   ami           = var.amis[var.aws_region]
   count = 2
--- output.tf
+++ output.tf
@@ -10,6 +10,7 @@ resource "local_file" "AnsibleInventory" {
  content = templatefile("inventory.tmpl",
  {
   k8s-master_ip = aws_instance.k8s-master.public_ip,
+  k8s-nfs_ip = aws_instance.k8s-nfs.public_ip,
   k8s-worker_ip = aws_instance.k8s-worker.*.public_ip,
   linux_user = var.linux_user
  }
